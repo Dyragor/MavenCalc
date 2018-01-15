7ec72d6436a037c35c48d81e2899a3a30977e286
Andreas <andreas.schranz22@gmail.com>
2018-01-15 08:33:35 +0100

    Finish Calculator and Tests

--- Taschenrechner/src/main/java/CalculatorApp.java
+++ Taschenrechner/src/main/java/CalculatorApp.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class CalculatorApp {
     public static void main(String[] args) {
-        boolean calculating = true;
+
         Calculator calc = new Calculator();
-        while(calculating){
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("(Start/Exit) -> ");
-            String input = scanner.nextLine();
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("1. Zahl ");
+        String input1 = scanner.nextLine();
+        System.out.print("2. Zahl ");
+        String input2 = scanner.nextLine();
+        System.out.println(calc.addition(Double.parseDouble(input1), Double.parseDouble(input2)));
+        scanner.close();
     }
 }
